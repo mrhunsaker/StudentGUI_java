c033f79130bc506e05616bae859f71ad84c72bbf
mrhunsaker <hunsakerconsulting@gmail.com>
2025-12-17 13:15:25 -0700

    javadoc updates

---
+++ target/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.studentgui.app"},{"l":"com.studentgui.apphelpers"},{"l":"com.studentgui.apphelpers.dto"},{"l":"com.studentgui.apppages"},{"l":"com.studentgui.apptheming"},{"l":"com.studentgui.bootstrap"},{"l":"com.studentgui.test"},{"l":"com.studentgui.tools"},{"l":"com.studentgui.uicomp"}];updateSearchResults();
